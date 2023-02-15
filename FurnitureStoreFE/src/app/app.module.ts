e80c69754832d09ba2dca1140d7ea0541b34e331
Azdevpp <Azdevpp@gmail.com>
2023-02-16 00:02:58 +0200

    basic navbar

--- FurnitureStoreFE/src/app/app.module.ts
+++ FurnitureStoreFE/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TooltipModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    TooltipModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ FurnitureStoreFE/src/app/public/public.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class PublicModule {}
