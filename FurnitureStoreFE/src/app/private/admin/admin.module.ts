3dce0b6c7a057d9b8feaa3a396d8718de432ddb4
Azdevpp <Azdevpp@gmail.com>
2023-02-22 19:23:24 +0200

    Distructure product folder

--- FurnitureStoreFE/src/app/private/admin/admin.module.ts
+++ FurnitureStoreFE/src/app/private/admin/admin.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { AddProductComponent } from './add-product/add-product.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
 import { PrivateRoutingModule } from '../private-routing.module';
-import { AllProductsComponent } from './all-products/all-products.component';
-import { EditProductsComponent } from './edit-products/edit-products.component';
+import { AllProductsComponent } from './products/all-products/all-products.component';
+import { EditProductsComponent } from './products/edit-products/edit-products.component';
 
 @NgModule({
-  declarations: [AdminDashboardComponent, AddProductComponent, AllProductsComponent, EditProductsComponent],
+  declarations: [
+    AdminDashboardComponent,
+    AddProductComponent,
+    AllProductsComponent,
+    EditProductsComponent,
+  ],
   imports: [CommonModule, PrivateRoutingModule],
   exports: [],
 })
--- FurnitureStoreFE/src/app/private/private-routing.module.ts
+++ FurnitureStoreFE/src/app/private/private-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
-import { AddProductComponent } from './admin/add-product/add-product.component';
-import { AllProductsComponent } from './admin/all-products/all-products.component';
-import { EditProductsComponent } from './admin/edit-products/edit-products.component';
+import { AddProductComponent } from './admin/products/add-product/add-product.component';
+import { AllProductsComponent } from './admin/products/all-products/all-products.component';
+import { EditProductsComponent } from './admin/products/edit-products/edit-products.component';
 
 const routes: Routes = [
   {
